50b1a44fec981b4771572deb8d295c6db9da2cdd
saif eddine askri <saif.askri@outlook.de>
2022-05-26 00:21:38 +0200

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,83 +1,58 @@
 import React, { useEffect, useState } from "react";
 import { NavLink, useHistory } from "react-router-dom";
-
+import './Navbar.css';
 const Navbar = ({ logout, token }) => {
 
-// FIXME:  
-
-  useEffect(()=>{
-    setInterval(()=>{
-
-    })
-  },[])
-
-
   return (
-
-
-    <nav className="navbar navbar-expand-lg navbar-dark" 
-        style={{
-          border: "none",
-          backgroundColor: "rebeccapurple",
-          borderRadius: "0 0 0 0"
-        }}
-     >
-      <span className="navbar-brand">Duravit</span>
-
-      <div className="collapse navbar-collapse" id="navbarColor01">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/">
-              Accueil
-            </NavLink>
-          </li>
-
-
-        </ul>
-        <ul className="navbar-nav ml-auto">
-
-
-          {!token ?
-            <>
-              <li className="nav-item">
-
-                <NavLink className="nav-link" to="/login">
-                  Se connecter
-                </NavLink>
-              </li>
-
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/register">
-                  S'enregistrer
-                </NavLink>
-              </li>
-         
-            </>
-
-            : <>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/account">
-                  Mon compte
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                 
-                <NavLink className="nav-link" to= "/admin/userList">
-                  Liste des Compte
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <button
-                  onClick={logout}
-                  className="btn Mywhitecolor">Déconnexion</button>
-
-
-              </li>
-            </>
-          }
-        </ul>
-      </div>
-    </nav>
+      <>
+        <nav className="navbar navbar-expand-md bg-primary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Duravit</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <NavLink className="nav-link text-white" to="/">Accueil</NavLink> 
+                </li>
+              </ul>
+              <div className="navbar-nav">
+                  {!token ?
+                      <>
+                        <li className="nav-item">
+                          <NavLink className="nav-link text-white" to="/login">
+                            Se connecter
+                          </NavLink>
+                        </li>
+                        <li className="nav-item">
+                          <NavLink className="nav-link text-white" to="/register">
+                            S'enregistrer
+                          </NavLink>
+                        </li>
+                      </>
+                          : 
+                      <>
+                        <li className="nav-item">
+                          <NavLink className="nav-link text-white" to="/account">
+                            Mon compte
+                          </NavLink>
+                        </li>
+                        <li className="nav-item">
+                          <NavLink className="nav-link text-white" to= "/admin/userList">
+                            Liste des Compte
+                          </NavLink>
+                        </li>
+                        <li className="nav-item">
+                          <button onClick={logout} className="btn Mywhitecolor">Déconnexion</button>
+                        </li>
+                      </>
+                    }
+                </div>
+            </div>
+          </div>
+        </nav>
+      </>
   );
 };
 
